5bad0e7c759cd6c7ca7b51527f54c9cbf66450fd
Mohsen Adeli <mohsen.adelie@gmail.com>
2020-05-31 02:07:59 +0430

    add @Primary to solve default bean injection

--- src/main/java/com/adeli/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/com/adeli/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,14 +1,18 @@
 package com.adeli.sfgdi.controllers;
 
 import com.adeli.sfgdi.services.GreetingService;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
-    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
+//    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
+//        this.greetingService = greetingService;
+//    }
+
+    // work with @Primary
+    public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
